d8ac89431f49a1d0c827b7f82f44c9b45a69b2b3
krisbitney <kristoferbitney@hotmail.com>
2022-04-26 18:12:55 -0500

    updated polywrap package versions

--- src/polywrap-utils/testUtils.ts
+++ src/polywrap-utils/testUtils.ts
@@ -2,10 +2,6 @@ import { ensPlugin } from '@web3api/ens-plugin-js'
 import { ethereumPlugin } from '@web3api/ethereum-plugin-js'
 import { ipfsPlugin } from '@web3api/ipfs-plugin-js'
 
-// "@web3api/ethereum-plugin-js": "0.0.1-prealpha.68",
-//   "@web3api/ipfs-plugin-js": "0.0.1-prealpha.68",
-//   "@web3api/ens-plugin-js": "0.0.1-prealpha.68",
-
 export function getPlugins(ethereum: string, ipfs: string, ensAddress: string) {
   return {
     redirects: [],
@@ -16,7 +12,7 @@ export function getPlugins(ethereum: string, ipfs: string, ensAddress: string) {
       },
       {
         uri: 'w3://ens/ens.web3api.eth',
-        plugin: ensPlugin({ addresses: { testnet: ensAddress } }),
+        plugin: ensPlugin({ query: { addresses: { testnet: ensAddress } } }),
       },
       {
         uri: 'w3://ens/ethereum.web3api.eth',
